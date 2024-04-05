1595541cd8aeaee98401b239c1281e3d65c93ee5
fakatveritus <faizan.khan@verituslabs.com>
2024-04-06 02:07:57 +0500

    Test comit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
   let myObj = {
-    username: "hitesh",
+    username: "React Tutorials",
     age: 21
   }
   let newArr = [1, 2, 3]
@@ -15,8 +15,8 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username="chaiaurcode" btnText="click me" />
-      <Card username="hitesh" />
+      <Card username="React Tutorials" btnText="click me" />
+      <Card username="Testing" />
     </>
   )
 }
